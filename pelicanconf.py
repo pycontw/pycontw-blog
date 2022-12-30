9b2693735778275fccd04906c2c5cec02ca3b320
Wei Lee <weilee.rx@gmail.com>
2022-12-30 12:24:58 +0800

    config: add Flex as theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,12 +1,42 @@
+# Site metadata
 AUTHOR = "PyCon Taiwan Organizers"
-SITENAME = "PyCon Taiwan"
+SITENAME = "PyCon Taiwan Blog"
+SITETITLE = "PyCon Taiwan"
+SITEDESCRIPTION = "PyCon Taiwan official blog. Catch up the latest announcement here!"
+# empty for local development
 SITEURL = ""
+SITELOGO = SITEURL + "/images/profile.jpg"
+# FAVICON = SITEURL + "/images/favicon.ico"
 
+
+# blog config
 PATH = "content"
-
+STATIC_PATHS = ["images", "extra"]
+DEFAULT_PAGINATION = 10
 TIMEZONE = "Asia/Taipei"
-
 DEFAULT_LANG = "zh"
+MAIN_MENU = True
+MENUITEMS = (
+    ("Archives", "/archives.html"),
+    ("Categories", "/categories.html"),
+    ("Tags", "/tags.html"),
+)
+
+
+# Blogroll
+LINKS = (("Official Website", "http://tw.pycon.org"),)
+
+
+# Social widget
+SOCIAL = (
+    ("facebook", "https://www.facebook.com/pycontw"),
+    ("instagram", "https://www.instagram.com/pycon.tw/"),
+    ("twitter", "https://twitter.com/#!/PyConTW"),
+    ("linkedin", "https://www.linkedin.com/company/pycontw/"),
+    ("github", "https://github.com/pycontw"),
+    ("youtube", "https://www.youtube.com/channel/UCHLnNgRnfGYDzPCCH8qGbQw"),
+)
+
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -15,24 +45,24 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# Blogroll
-LINKS = (
-    ("Pelican", "https://getpelican.com/"),
-    ("Python.org", "https://www.python.org/"),
-    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
-    ("You can modify those links in your config file", "#"),
-)
 
-# Social widget
-SOCIAL = (
-    ("You can add links in your config file", "#"),
-    ("Another social link", "#"),
-)
-
-STATIC_PATHS = ["images", "extra"]
+# Theme Setting
+THEME = "theme/Flex"
+PLUGINS = ["i18n_subsites"]
+PYGMENTS_STYLE = "default"
+# JINJA_ENVIRONMENT = {"extensions": ["jinja2.ext.i18n"]}
+ROWSER_COLOR = "#333"
 
 
-DEFAULT_PAGINATION = 10
+# License
+CC_LICENSE = {
+    "name": "Creative Commons Attribution-ShareAlike",
+    "version": "4.0",
+    "slug": "by-sa",
+}
+COPYRIGHT_NAME = "PyCon Taiwan"
+COPYRIGHT_YEAR = 2023
+
 
 # Uncomment following line if you want document-relative URLs when developing
 # RELATIVE_URLS = True
